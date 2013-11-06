c239fdde8f651e69677ce53a638be1469ddc5a7b
Orr <outsmaingit@gmail.com>
2013-11-06 15:17:41 +0700

    add WIFI service

--- sql/subsstat.sql
+++ sql/subsstat.sql
@@ -22,4 +22,4 @@ insert into tbl_subsstat(start_date,end_date,node_ip,node_name,service,sub_servi
 insert into tbl_subsstat(start_date,end_date,node_ip,node_name,service,sub_service,prov_subs,conn_subs,min_line) VALUES('2013-11-05 00:00:01','2013-11-06 00:00:01','10.1.1.3','name3','GPON',NULL,33,44,1.03);
 insert into tbl_subsstat(start_date,end_date,node_ip,node_name,service,sub_service,prov_subs,conn_subs,min_line) VALUES('2013-11-01 00:00:01','2013-11-02 00:00:01','10.1.1.1','name1','DSL',NULL,44,55,1.04);
 insert into tbl_subsstat(start_date,end_date,node_ip,node_name,service,sub_service,prov_subs,conn_subs,min_line) VALUES('2013-11-03 00:00:01','2013-11-04 00:00:01','10.1.1.2','name2','DOCSIS',NULL,55,66,1.05);
-insert into tbl_subsstat(start_date,end_date,node_ip,node_name,service,sub_service,prov_subs,conn_subs,min_line) VALUES('2013-11-05 00:00:01','2013-11-06 00:00:01','10.1.1.3','name3','GPON',NULL,66,77,1.06);
+insert into tbl_subsstat(start_date,end_date,node_ip,node_name,service,sub_service,prov_subs,conn_subs,min_line) VALUES('2013-11-05 00:00:01','2013-11-06 00:00:01','10.1.1.3','name3','WIFI',NULL,66,77,1.06);
