04926883090b46e902806832bb6e0713c92ef783
Orr <outsmaingit@gmail.com>
2014-02-11 20:19:06 +0700

    Pre-defined data for ATTRIB_LIST

--- sql/ATTRIB_LIST.sql
+++ sql/ATTRIB_LIST.sql
@@ -6,6 +6,9 @@ CREATE TABLE `ATTRIB_LIST` (
   `NAME` varchar(128) DEFAULT NULL,
   `DESCRIPTION` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 
+insert into `ATTRIB_LIST`(`ID`,`ATTRIB_TYPE_ID`,`NAME`,`DESCRIPTION`) values (1,2,'IP Addr/mask',null);
+insert into `ATTRIB_LIST`(`ID`,`ATTRIB_TYPE_ID`,`NAME`,`DESCRIPTION`) values (2,3,'MAC Address',null);
+insert into `ATTRIB_LIST`(`ID`,`ATTRIB_TYPE_ID`,`NAME`,`DESCRIPTION`) values (3,2,'Gi-Addr',null);
