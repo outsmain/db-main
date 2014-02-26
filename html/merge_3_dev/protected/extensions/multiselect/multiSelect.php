4bbf4035589b34f0b45e5244939d0ccf40dfb819
nuengcpe <nueng.me@gmail.com>
2014-02-26 16:01:16 +0700

    merge 3 dev to dev-main
    add extension folder

---
+++ html/merge_3_dev/protected/extensions/multiselect/multiSelect.php
@@ -0,0 +1,40 @@
+<?php
+class multiSelect 
+{
+	public function __construct($params = array()){
+		foreach($params as $p => $v) $this->$p = $v;
+	}
+	
+	protected static function registerScript(){
+		$asset = dirname(__FILE__).DIRECTORY_SEPARATOR;
+		$baseUrl = Yii::app()->assetManager->publish($asset);
+
+		$cs = Yii::app()->clientScript;
+		//$cs->registerCoreScript('jquery');
+		$cs->registerScriptFile($baseUrl.'/jquery.multiselect.min.js', CClientScript::POS_HEAD);
+		$cs->registerScriptFile($baseUrl.'/jquery.multiselect.filter.min.js', CClientScript::POS_HEAD);
+		$cs->registerCssFile($baseUrl.'/css/jquery.multiselect.css');
+		$cs->registerCssFile($baseUrl.'/css/jquery.multiselect.filter.css');
+	}
+
+	protected static function create($selector, $options = array(), $position = CClientScript::POS_END){
+		self::registerScript();
+		$options = CJavaScript::encode(array_merge(self::defaultOptions(),$options));
+		Yii::app()->clientScript->registerScript(__CLASS__.$selector, 'jQuery("'.$selector.'").multiselect('.$options.').multiselectfilter();', $position);
+	}
+	
+	public function addMultiselect($selector, $ops = array()){
+		self::create($selector, array_merge(self::defaultOptions(),$ops));
+	}
+
+	protected static function defaultOptions(){
+		$configs = array(
+			'multiple' => false,
+			'selectedList' => 1,
+			'minWidth' => 150,
+			'height' => 'auto',
+		);
+		return $configs;
+	}
+}
+?>
---
+++ html/merge_3_dev/protected/extensions/timepicker/views/default.php
@@ -0,0 +1,3 @@
+<? 
+echo CHtml::activeTextField($this->model,$this->id,array('width'=>100,'maxlength'=>100,"placeholder"=>"DD/MM/YYYY HH:MM:SS","class"=>"timepicker","value"=>$this->model->{$this->name}?$this->model->{$this->name}:$this->options['value']));
+?>
